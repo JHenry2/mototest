29c7d1fb0c9971a5948d885ff0ababd9be92afa7
jh1261 <jacob.henry@unh.edu>
2022-11-09 19:37:35 -0500

    update to match local

--- test_main.py
+++ test_main.py
@@ -3,9 +3,10 @@ import os
 import s3cp
 
 
-with open("hello.moto", "rb") as f:
-    object_name = os.path.basename(f.name)
-exception_or_True = s3cp.test_file_to_cloud("hello.moto", "mybucket", 
-"test")
-print(f"Upload Successful: {exception_or_True}")
+def test_mock():
+	with open("hello.moto", "rb") as f:
+	    object_name = os.path.basename(f.name)
+	exception_or_True = s3cp.test_file_to_cloud("hello.moto", "mybucket", 
+	"test")
+	print(f"Upload Successful: {exception_or_True}")
 
